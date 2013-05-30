02a3b2a99681ae93ed063024cef98624cdaa8159
zhcpkl <zhcpkl@gmail.com>
2013-05-30 09:51:04 -0700

    added create_table and index_files db commands

---
+++ bin/db_tasks.py
@@ -0,0 +1,83 @@
+import sqlite3
+import os
+import sys
+import datetime
+
+"""
+This file handles all indexing of database information
+
+TODO: directory information => make dictionary?
+"""
+
+
+def create_table():
+    """
+    Run create table script
+
+    TODO: 1. check if table exists with python before running script? 
+            (script auto-deletes existing table)
+    """
+    # set up directories
+    db_folder = os.path.join(os.path.dirname(os.getcwd()),'db')
+    schema_file = os.path.join(db_folder, 'schema.sql')
+    db_file = os.path.join(db_folder, 'worldmap.db')
+
+    # run schema file
+    con = sqlite3.connect(db_file)
+    with con:
+        cur = con.cursor() 
+        sql = open(schema_file, 'r').read()  
+        cur.executescript(sql)
+    con.close()
+
+
+def index_files():
+    """
+    Find all images and update information for them in the database
+
+    TODO => TEST to see if update/insert command is working correctly
+    """
+    # set up directories
+    db_folder = os.path.join(os.path.dirname(os.getcwd()),'db')
+    db_file = os.path.join(db_folder, 'worldmap.db')
+    resources_folder = os.path.join(os.path.dirname(os.getcwd()),'resources')
+
+    # get list of folders with user journeymap information
+    mappers_folders = os.listdir(resources_folder)
+    
+    # set up sql connection
+    con = sqlite3.connect(db_file)
+    with con:
+        cur = con.cursor() 
+
+        # look in each user's jm folder
+        for folder in mappers_folders:
+            full_path = os.path.join(resources_folder, folder)
+            images = os.listdir(full_path)
+            images = sorted(images, key=lambda im: os.path.getsize(os.path.join(full_path,im)))
+            for im in images:
+                file_path = os.path.join(full_path,im)
+                bytes = os.path.getsize(file_path)
+                date_modified = os.path.getmtime(file_path)
+
+                # insert or replace sql
+                qry_template = """
+                insert or replace into source_files 
+                (username, filename, file_path, file_size_in_bytes, image_modified_date) 
+                values (?, ?, ?, ?, ?);
+                """
+                info_tupe = (folder, im, full_path, bytes, date_modified)
+                cur.execute(qry_template, info_tupe)
+    con.close()
+
+
+def main():
+    for arg in sys.argv:
+        if arg == 'create_table':
+            create_table()
+        if arg == 'index_files':
+            index_files()
+
+
+if __name__ == "__main__":
+    main()
---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+-- BASED ON SQLite3 type 'affinity' convention
+DROP TABLE IF EXISTS source_files;
+CREATE TABLE source_files (
+	username TEXT, 
+	-- taargus_
+	filename TEXT, 
+	-- 1,1_Normal.region.png
+	file_path TEXT,
+	-- /bin/resources/taargus_/
+	file_size_in_bytes INTEGER,
+	-- 2112
+	image_modified_date NUMERIC, 
+	-- modified
+	UNIQUE (username, filename) ON CONFLICT REPLACE
+);
