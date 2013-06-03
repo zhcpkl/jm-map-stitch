4deac960c262732ebad7b8cbff6ec340ef346bda
zhcpkl <zhcpkl@gmail.com>
2013-06-03 14:01:48 -0700

    added gdal2tiles.py for slicing huge image for openlayers viewing

--- bin/map_maker.py
+++ bin/map_maker.py
@@ -12,4 +12,21 @@ def find_recent_changes():
     """
     Find files that have been updated since
     the last image merge. Don't repeat merges
+
+    TODO: Find all images that aren't blank.
+    See if they have been compiled. 
+    If they haven't been compiled. Compile them
+    for the first time. Otherwise, add file to the
+    aggregated image.
     """
+    # set up directories
+    # db_folder = os.path.join(os.path.dirname(os.getcwd()),'db')
+    # db_file = os.path.join(db_folder, 'worldmap.db')
+    # con = sqlite3.connect(db_file)
+
+    # with con:
+    #     cur = con.cursor() 
+    #     cur.execute(sql)
+    # con.close()
+
+    return None
